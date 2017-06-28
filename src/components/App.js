14fe32f117f334355e4961cd367478d3c104c23d
Bulat F <write2bulat@gmail.com>
2017-06-28 21:27:16 +0300

    Initialize repository
    - Create react app
    - Install `redux` and `react-redux` npm packages
    - Clean up basic component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Insurance App</h2>
+          <h3>Powered by <a href="https://docs.vericred.com">vericred.com</a></h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
